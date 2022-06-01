e626a001e3ae9237b345d4bc53cdbc237eec1f1b
Suman Bera <suman80bera@gmail.com>
2022-06-01 19:50:18 +0530

    Final

---
+++ src/validator/formatter.js
@@ -0,0 +1,16 @@
+const trim=function(){
+    const a="   Function   "
+    console.log(a.trim())
+}
+const changetoLowerCase=function(){
+ const b="MY NAME IS SUMAN BERA"
+ console.log(b.toLowerCase())
+}
+const changeToUpperCase=function(){
+    const c=" i am form siliguri"
+    console.log(c.toUpperCase())
+}
+
+module.exports.trim=trim
+module.exports.changetoLowerCase=changetoLowerCase
+module.exports.changeToUpperCase=changeToUpperCase
