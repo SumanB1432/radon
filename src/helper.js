bded6833935a73a26aff27366613d656389828af
Suman Bera <suman80bera@gmail.com>
2022-06-01 17:22:28 +0530

    Fifth commit

---
+++ src/helper.js
@@ -0,0 +1,9 @@
+const date=01
+const month=06
+const getBatchInfo=function(){
+    console.log("Radon,w3D3,today topic is nodejs")
+
+}
+module.exports.date=date
+module.exports.month=month
+module.exports.getBatchInfo=getBatchInfo
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const externalModule = require('./logger')
+const externalModule2=require('./helper')
 
 const router = express.Router();
 
@@ -7,7 +8,11 @@ router.get('/test-me', function (req, res) {
     console.log('The constant in logger route has a value '+externalModule.endpoint)
     console.log('The current batch is '+externalModule.batch)
     externalModule.log()
+    externalModule.welcome()
+    console.log("Date:"+externalModule2.date)
+    console.log("Month:"+externalModule2.month)
     res.send('My first ever api!')
+   
 });
 
 router.get('/test-me1', function (req, res) {
