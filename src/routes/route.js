2b7773b18caebc5458072efd90b69b7e181494fd
Suman Bera <suman80bera@gmail.com>
2022-06-08 22:48:22 +0530

    part 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,8 @@ const router = express.Router();
 // const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 const AuthorController=require("../controllers/authorController")
+const BookModel=require("../models/bookModel")
+const AuthorModel=require("../models/authorModel")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -22,6 +24,41 @@ router.get("/getAuthorsData", AuthorController.getAuthorsData)
 router.get("/authorId",AuthorController.authorid)
 router.get("/authorOfBook",AuthorController.authorOfBook)
 router.get("/bookPrice",AuthorController.bookPrice)
+
+router.get("/bookByAuthor/:author_id",  async function(req,res)
+{
+    let data = req.params.author_id;
+    let bookByAuthor = await BookModel.find({author_id:data}).select({name:1,_id:0})
+    res.send({data:bookByAuthor})
+})
+router.get("/author",async function(req,res){
+    let data1= await AuthorModel.find({age:{$gt:50}})
+    // console.log(data1)
+    
+    let data2 = await BookModel.find()
+    // console.log(data2)
+    let author1=[]
+    data1.map(item=>{
+        let author=[];
+        data2.map(item1=>{
+            if((item.author_id==item1.author_id) && (item1.ratings>=4)){
+            author.push(item.author_name);
+            author.push(item.age);
+            } 
+            // console.log(author);
+        })
+        if(author.length>0) author1.push(author);
+
+    })
+    
+    res.send({data:author1})
+    
+    
+    
+})
+
+
+
 
 
 
