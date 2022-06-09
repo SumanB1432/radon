3dc582f70c0f2fc0322b4d9a33315755babb00fe
Suman Bera <suman80bera@gmail.com>
2022-06-09 12:44:42 +0530

    final

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const authorSchema = new mongoose.Schema({
     author_id:{
-       type: Number,
+       type: String,
        required:true
     },
     author_name:String,
