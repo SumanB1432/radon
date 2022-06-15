1a3dadb51875c7c8e433e1648d472ba9fedcb640
Suman Bera <suman80bera@gmail.com>
2022-06-15 14:16:20 +0530

    auth2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Suman-1432:Suman1432@cluster0.bkkfmpr.mongodb.net/suman-auth", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,31 @@
+const jwt = require("jsonwebtoken")
+const userModel =require("../models/userModel")
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
-    next()
+    let token = req.headers
+    if(!token){
+        res.sen({status:false,msg:"importent header is missing"})
+    }
+   next()
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+const authorise = async function(req, res, next) {
+    let logInuserId = req.params.UserId
+    let user = await userModel.findById(logInuserId)
+    let decodedToken = jwt.verify(token,"functionup-radium")
+    let decodedUserId = decodedToken.UserId
+    if(!user){
+        res.send({status:false,msg:"no such user found"})
+    }
+    else{
+    if(decodedUserId!==user._id){
+        res.send({status:false,msg:"User logged is not allowed to modify the requested users data"})
+    }
+    
     next()
 }
+}
+module.exports.authenticate= authenticate
+module.exports.authorise=authorise
